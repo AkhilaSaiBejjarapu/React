47530a1776484e3538b4704d4deb7ff7764bed35
Koppala Naveenkumar <naveenkumarkoppala@gmail.com>
2021-12-07 18:58:46 +0530

    files

---
+++ reactall_sample.js
@@ -0,0 +1,7 @@
+const element = (
+    <div>
+        <h1 className="styling">NESTED REACT JSX</h1>
+        <p className="details">AKHILA SAI<br />FrontEnd Developer</p>
+    </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
